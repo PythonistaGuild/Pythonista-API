00ab7265d875a05322a3cfb0c9be4deee9e02fde
EvieePy <evieepy@gmail.com>
2023-06-13 09:36:56 +1000

    Add launcher.py

---
+++ launcher.py
@@ -0,0 +1,42 @@
+"""MIT License
+
+Copyright (c) 2023 PythonistaGuild
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import asyncio
+
+import aiohttp
+import uvicorn
+
+import core
+import api
+
+
+async def main() -> None:
+    async with aiohttp.ClientSession() as session, core.Database() as database:
+        app: api.Server = api.Server(session=session, database=database)
+
+        config = uvicorn.Config(app, port=core.config['SERVER']['port'])
+        server = uvicorn.Server(config)
+        await server.serve()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
