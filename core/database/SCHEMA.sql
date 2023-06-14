21c975845c1ea426fcb7520b7365a5132fb0928e
EvieePy <evieepy@gmail.com>
2023-06-14 18:57:10 +1000

    Allow 512 chars in token description.

--- core/database/SCHEMA.sql
+++ core/database/SCHEMA.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS tokens (
     tid SERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users(uid),
     token_name VARCHAR(32) NOT NULL CHECK (LENGTH(token_name) > 2),
-    token_description VARCHAR(256),
+    token_description VARCHAR(512),
     token TEXT NOT NULL,
     verified BOOLEAN NOT NULL DEFAULT false,
     uses INTEGER NOT NULL DEFAULT 0,
